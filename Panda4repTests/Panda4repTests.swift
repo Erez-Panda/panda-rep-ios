299df86099de18355189983069702689659e52a8
Erez <erez.haim@outlook.com>
2014-12-16 13:23:10 +0200

    Initial Commit

---
+++ Panda4repTests/Panda4repTests.swift
@@ -0,0 +1,36 @@
+//
+//  Panda4repTests.swift
+//  Panda4repTests
+//
+//  Created by Erez on 12/16/14.
+//  Copyright (c) 2014 Erez. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Panda4repTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
